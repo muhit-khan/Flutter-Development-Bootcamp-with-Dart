af4c594b4bb5344c910f5ae3e7e7380182f64a11
Muhit Khan <muhitkhan_bd@outlook.com>
2022-10-29 16:09:38 +0530

    Update main.dart

--- APP_I_AM_RICH/iamrich/lib/main.dart
+++ APP_I_AM_RICH/iamrich/lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Hello World')));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text("I Am Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/Diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
