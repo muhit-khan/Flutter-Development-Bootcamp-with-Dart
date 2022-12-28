b878867597d3f57cc0aac0023d25b57057142ead
Muhit Khan <muhitkhan_bd@outlook.com>
2022-12-28 23:50:22 +0530

    Update main.dart

--- APP_Destiny/destiny/lib/main.dart
+++ APP_Destiny/destiny/lib/main.dart
@@ -15,3 +15,17 @@ class Destiny extends StatelessWidget {
     );
   }
 }
+
+class StoryPage extends StatefulWidget {
+  const StoryPage({super.key});
+
+  @override
+  State<StoryPage> createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
