639c9bbb6b9502271e669d9c4fdd79e03b15905d
Muhit Khan <muhitkhan_bd@outlook.com>
2022-12-28 23:49:16 +0530

    Update main.dart

--- APP_Destiny/destiny/lib/main.dart
+++ APP_Destiny/destiny/lib/main.dart
@@ -8,14 +8,9 @@ class Destiny extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+      title: 'Story Page',
+      theme
+      home: StoryPage(),
       ),
     );
   }
