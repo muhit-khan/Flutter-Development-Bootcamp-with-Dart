b19df632176309794fee3a7e16a8f6e26e554803
Muhit Khan <muhitkhan_bd@outlook.com>
2023-01-04 22:16:35 +0530

    Update main.dart

--- APP_BMI_Calculator/bmicalculator/lib/main.dart
+++ APP_BMI_Calculator/bmicalculator/lib/main.dart
@@ -20,8 +20,6 @@ class MyApp extends StatelessWidget {
         // changing the primarySwatch below to Colors.green and then invoke
         // "hot reload" (press "r" in the console where you ran "flutter run",
         // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
