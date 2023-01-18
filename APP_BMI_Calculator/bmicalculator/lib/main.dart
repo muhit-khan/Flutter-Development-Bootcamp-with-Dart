1d3f90d7b4368a5e953bec5ebdf5b8118ec03746
Muhit Khan <muhitkhan_bd@outlook.com>
2023-01-18 13:50:53 +0530

    Update main.dart

--- APP_BMI_Calculator/bmicalculator/lib/main.dart
+++ APP_BMI_Calculator/bmicalculator/lib/main.dart
@@ -68,8 +68,6 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
